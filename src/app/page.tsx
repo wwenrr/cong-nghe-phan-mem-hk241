6dd0cf08028fe66ca4b7b491fd673204dc5de1f1
MK <qscvdefb@gmail.com>
2024-09-24 16:32:38 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import Nav from "./nav";
 import "@styles/home.scss"
 
-interface ITuple {
+interface tuble {
   name: string,
   url: string
 }
 
-function Url() {
-
+function Url(arr:Array<tuble>) {
+  arr.map(() => {
+    return (
+      <div className="">hi</div>
+    )
+  })
 }
 
 export default function Home() {
   return (
     <>
       <div className="warp">
-
+          
       </div>
     </>
   );
