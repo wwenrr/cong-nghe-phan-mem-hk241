48e6821b65d38badb5525640e883fa48f10c57a9
MK <qscvdefb@gmail.com>
2024-09-24 17:03:43 +0700

    .

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,6 +4,11 @@ import '@styles/header.scss'
 export default function Header() {
     return (
     <>
+        <div className="title">
+            <span>ĐẠI HỌC QUỐC GIA TP.HCM</span>
+            <span>TRƯỜNG ĐẠI HỌC BÁCH KHOA</span>
+        </div>
+
         <div className="cta_a">
             <a className="active" href="https://github.com/wwenrr/cong-nghe-phan-mem-hk241/blob/main/README.md" target="_blank" >Dự Án</a>
             <a className="active" href="https://github.com/wwenrr/cong-nghe-phan-mem-hk241/blob/main/README.md" target="_blank">Dịch Vụ</a>
